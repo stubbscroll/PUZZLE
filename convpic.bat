3b6259d626c44d37bbe0510d70faeeca106e72e8
Ruben Spaans <spaans@pvv.org>
2012-07-16 02:59:59 +0200

    added screenshot to autosolve

---
+++ convpic.bat
@@ -0,0 +1,13 @@
+move aka*.bmp pic
+move has*.bmp pic
+move hey*.bmp pic
+move hit*.bmp pic
+move mas*.bmp pic
+move nur*.bmp pic
+move pic*.bmp pic
+move sli*.bmp pic
+move yaj*.bmp pic
+cd pic
+for %%s in (*.bmp) do convert %%s %%~ns.png
+del *.bmp
+cd ..
